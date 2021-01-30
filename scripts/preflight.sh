018dbe47c9df40904aa3a3f132ca5844b23ae1d3
ironoa <alessio.onori@gmail.com>
2021-01-30 15:27:17 +0100

    cert manager fix

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -2,5 +2,5 @@
 
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.2/cert-manager.crds.yaml
 
-#kubectl label namespace default certmanager.k8s.io/disable-validation="true" --overwrite=true
-kubectl create namespace cert-manager || true
+kubectl label namespace default certmanager.k8s.io/disable-validation="true" --overwrite=true
+#kubectl create namespace cert-manager || true
