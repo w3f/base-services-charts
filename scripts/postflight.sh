311c6a6409c6ab1789991c21806594fceef9d007
Philipp D <philipp@web3.foundation>
2021-12-28 12:53:10 -0600

    Delete postflight.sh

--- scripts/postflight.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/heptio/velero/releases/download/v0.10.0/ark-v0.10.0-linux-amd64.tar.gz
-tar -xvzf ark-v0.10.0-linux-amd64.tar.gz
-./ark plugin add quay.io/stackpoint/ark-blockstore-digitalocean:v0.10.0 || true
